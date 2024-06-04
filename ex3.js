ad2bd5c0accf51b8b3eaaf1101c75e951adc0afa
Guilherme <134329673+guicamarg0@users.noreply.github.com>
2024-06-04 17:18:30 -0300

    Add files via upload

---
+++ ex3.js
@@ -0,0 +1,9 @@
+        let linhas=0
+        let espaco=2
+        let img="*"
+        while (linha < 3) {
+            linha += 1
+            for (espaco=2;espaco >0; espaco--){
+                console.log(img)
+            }
+        }
